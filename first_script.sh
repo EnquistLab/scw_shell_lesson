afecde6b6d1ecd660490f991d23e6a19d6d069da
iskanderun <iskanderdun@gmail.com>
2017-02-25 12:30:23 -0700

    Add help function

--- first_script.sh
+++ first_script.sh
@@ -1,3 +1,13 @@
-head -5 animals.txt | sort > animals_first5.txt
-wc -c animals_first5.txt > animals_first5_size.txt
+#this script is used to count the number of characters in a file
 
+
+#head -5 animals.txt | sort > animals_first5.txt
+#wc -c animals_first5.txt > animals_first5_size.txt
+
+if [ "$1" == "-h"] || [ "$1" == "--help"]; then
+   echo "Usage: sh $0 input_file output_file"
+   exit
+fi
+
+# This script is used to count the number of characters in a file
+head -5 $1 | sort | wc -c > $2
